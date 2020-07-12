8b779f5365c7c6f95777a84f52a2d0fcacdf39ed
ohakutsu <hntkaaau0842pon@yahoo.co.jp>
2020-07-12 22:51:38 +0900

    Create index page

---
+++ components/molecules/ProfileImage.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ProfileImage = styled.img({
+  width: '100px',
+  height: '100px',
+  borderRadius: '50%'
+})
---
+++ components/templates/Footer.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const Footer: React.FC = ({ children }) => (
+  <footer className='footer'>{children}</footer>
+)
