9d6105aae5eb9569711afe9e8cc7508fd87e90d1
ohakutsu <hntkaaau0842pon@yahoo.co.jp>
2020-07-13 21:36:00 +0900

    Add datas

---
+++ datas/links.ts
@@ -0,0 +1,14 @@
+export type Links = Link[]
+
+export type Link = {
+  url: string
+  title: string
+}
+
+export const links: Links = [
+  { url: 'https://www.facebook.com/ohakutsu', title: 'Facebook' },
+  { url: 'https://github.com/ohakutsu', title: 'GitHub' },
+  { url: 'https://www.linkedin.com/in/ohakutsu/', title: 'LinkedIn' },
+  { url: 'https://qiita.com/ohakutsu', title: 'Qiita' },
+  { url: 'https://twitter.com/ohakutsu', title: 'Twitter' }
+]
